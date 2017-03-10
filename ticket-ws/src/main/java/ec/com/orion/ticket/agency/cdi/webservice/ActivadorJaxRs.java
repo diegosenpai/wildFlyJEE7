bb0f89209a68c0471f78bd455b972e69e1a89e63
diegosenpai <dmalmeida@gmail.com>
2017-03-09 21:03:03 -0500

    Eliminar versiones anteriores de proyecto ticket-ws y su cliente desactualizadas.

--- ticket-ws/src/main/java/ec/com/orion/ticket/agency/cdi/webservice/ActivadorJaxRs.java
+++
@@ -1,9 +0,0 @@
-package ec.com.orion.ticket.agency.cdi.webservice;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class ActivadorJaxRs extends Application {
-
-}
